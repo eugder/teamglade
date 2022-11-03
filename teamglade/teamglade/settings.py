94799c315417cedea4ac5ac327e332bab29f3bd7
eugder <eugder@gmail.com>
2022-11-03 18:51:52 +0200

    PostgreSQL connection added

--- teamglade/teamglade/settings.py
+++ teamglade/teamglade/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'teamglade.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'teamglade_db',
+        'USER': 'tg_admin',
+        'PASSWORD': '123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
