04f1544a563cf654fd5d1147376f4c3659928f34
eugder <eugder@gmail.com>
2023-07-26 13:51:11 +0300

    First view added

--- teamglade/rooms/views.py
+++ teamglade/rooms/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 def index(request):
-    return HttpResponse("Hello, world")
+    rooms = Room.objects.all()
+    room_row = []
+
+    for r in rooms:
+        room_row.append(str(r.created_by) + '   ' + r.name)
+
+    responce_html = '<br>'.join(room_row)
+
+    return HttpResponse(responce_html)
