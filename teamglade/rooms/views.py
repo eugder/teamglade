34c1debf29a0636dbce9a2b0f9069995e79226c9
eugder <eugder@gmail.com>
2023-07-31 13:41:27 +0300

    Other view variant added

--- teamglade/rooms/views.py
+++ teamglade/rooms/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 
 from .models import Topic, Room
 
 def room(request):
     topics_list = Topic.objects.all().values()
-    template = loader.get_template('room.html')
-    context = {'topics_list' : topics_list}
+    context = {'topics_list': topics_list}
+    return render(request, 'room.html', context)
 
-    return HttpResponse(template.render(context, request))
+    # topics_list = Topic.objects.all().values()
+    # template = loader.get_template('room.html')
+    # context = {'topics_list' : topics_list}
+    #
+    # return HttpResponse(template.render(context, request))
 
 def index(request):
     rooms = Room.objects.all()
