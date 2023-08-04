8f9cb6a5ecd0169c96f338e350ac214fe2d0f4d6
eugder <eugder@gmail.com>
2023-08-04 14:01:00 +0300

    Other view variant added

--- teamglade/rooms/tests.py
+++ teamglade/rooms/tests.py
@@ -1,3 +1,8 @@
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+class RoomViewTests(TestCase):
+    def test_room_view_status_code(self):
+        url = reverse('room')
+        responce = self.client.get(url)
+        self.assertEquals(responce.status_code, 200)
--- teamglade/rooms/views.py
+++ teamglade/rooms/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import loader
+#from django.urls import reverse
 
 from .models import Topic, Room
 
 def room(request):
     topics_list = Topic.objects.all().values()
     context = {'topics_list': topics_list}
+    #print(reverse('room'))
     return render(request, 'room.html', context)
 
     # topics_list = Topic.objects.all().values()
