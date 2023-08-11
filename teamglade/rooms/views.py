f63e69e7d98316e88657d568a84603efa4abd4f8
eugder <eugder@gmail.com>
2023-08-11 13:58:59 +0300

    Home page added

--- teamglade/rooms/views.py
+++ teamglade/rooms/views.py
@@ -18,12 +18,4 @@ def room(request):
     # return HttpResponse(template.render(context, request))
 
 def index(request):
-    rooms = Room.objects.all()
-    room_row = []
-
-    for r in rooms:
-        room_row.append(str(r.created_by) + '   ' + r.name)
-
-    responce_html = '<br>'.join(room_row)
-
-    return HttpResponse(responce_html)
+    return render(request, 'index.html')
