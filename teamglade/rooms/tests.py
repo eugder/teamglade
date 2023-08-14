1bf11c53a05ed264469e5a538db14717232f8b19
eugder <eugder@gmail.com>
2023-08-14 13:58:59 +0300

    New test added

--- teamglade/rooms/tests.py
+++ teamglade/rooms/tests.py
@@ -1,6 +1,12 @@
 from django.urls import reverse
 from django.test import TestCase
 
+class HomeViewTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        responce = self.client.get(url)
+        self.assertEquals(responce.status_code, 200)
+
 class RoomViewTests(TestCase):
     def test_room_view_status_code(self):
         url = reverse('room')
