c787674bb55ac30674b7b73695a8c0491bd9db55
eugder <eugder@gmail.com>
2023-08-21 14:01:17 +0300

    Room template changed, new topic view started

--- teamglade/rooms/urls.py
+++ teamglade/rooms/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('rooms/', views.room, name='room'),
+    path('topic/<int:pk>/new/', views.new_topic, name='new_topic'),
 ]
--- teamglade/rooms/views.py
+++ teamglade/rooms/views.py
@@ -7,17 +7,11 @@ from .models import Topic, Room, RoomUser
 
 def room(request):
     topics_list = Topic.objects.all()
-    # user_name = RoomUser.objects.get(id=1)
-    # print(user_name.username)
     context = {'topics_list': topics_list, 'user_name': "My User Name", 'room_name': "My Room"}
-    #print(context)
     return render(request, 'room.html', context)
 
-    # topics_list = Topic.objects.all().values()
-    # template = loader.get_template('room.html')
-    # context = {'topics_list' : topics_list}
-    #
-    # return HttpResponse(template.render(context, request))
+def new_topic(request, pk):
+    pass
 
 def index(request):
     return render(request, 'index.html')
