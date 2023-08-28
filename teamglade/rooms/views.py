f4d5c8cc9942475c8225e0f9392927386938ebbd
eugder <eugder@gmail.com>
2023-08-28 14:02:07 +0300

    New Topic form added

--- teamglade/rooms/views.py
+++ teamglade/rooms/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import loader
-#from django.urls import reverse
-
 from .models import Topic, Room, RoomUser
 
 def room(request):
