92859a7be46ab081fc1011c67de4c39bbb28a9a7
eugder <eugder@gmail.com>
2023-09-04 14:03:14 +0300

    New Topic Form class fixed

--- teamglade/rooms/forms.py
+++ teamglade/rooms/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 
 class NewTopicForm(forms.Form):
-    title = forms.CharField(lable='Title', max_length=100)
-    message = forms.CharField(label='Message', max_length=4000)
+    title = forms.CharField(label='Title', max_length=100)
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 5, 'placeholder': 'What is on your mind?'}),
+        label='Message',
+        max_length=4000,
+        help_text='The max length of the text is 4000.',
+    )
