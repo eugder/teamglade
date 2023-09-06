57d796b08d76a139289a7c0f5ebdf5080cb1f834
eugder <eugder@gmail.com>
2023-09-06 14:03:59 +0300

    django-widget-tweaks Template form added

--- teamglade/rooms/forms.py
+++ teamglade/rooms/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
 class NewTopicForm(forms.Form):
-    title = forms.CharField(label='Title', max_length=100)
+    title = forms.CharField(label='Title', max_length=160)
     message = forms.CharField(
         widget=forms.Textarea(attrs={'rows': 5, 'placeholder': 'What is on your mind?'}),
         label='Message',
-        max_length=4000,
-        help_text='The max length of the text is 4000.',
+        max_length=1000,
+        help_text='The max length of the text is 1000.',
     )
