4e4ba10a08fe30830bef63b79eb6f238754aea02
eugder <eugder@gmail.com>
2023-09-22 13:58:55 +0300

    navbar and signup improved

--- teamglade/accounts/views.py
+++ teamglade/accounts/views.py
@@ -1,11 +1,14 @@
-#from django.contrib.auth import login
+from django import forms
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from rooms.models import RoomUser
 
 class RoomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
+        email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
         model = RoomUser
+        fields = ('username', 'email', 'password1', 'password2')
         #fields = UserCreationForm.Meta.fields + ('custom_field',) - adding fields don't working
 
 def signup(request):
@@ -13,7 +16,7 @@ def signup(request):
         form = RoomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            #login(request, user)
+            login(request, user)
             return redirect('home')
     else:
         form = RoomUserCreationForm()
