af28d1cab38ddb13b83ad3dbdfc1ea62dcd878c9
eugder <eugder@gmail.com>
2023-10-06 14:02:00 +0300

    more signup tests added

---
+++ teamglade/accounts/tests/test_form_signup.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from ..views import RoomUserCreationForm
+
+class SignUpFormTest(TestCase):
+    def test_form_has_fields(self):
+        # Form has all fields
+        form = RoomUserCreationForm()
+        expected = ['username', 'email', 'password1', 'password2',]
+        actual = list(form.fields)
+        self.assertSequenceEqual(expected, actual)
