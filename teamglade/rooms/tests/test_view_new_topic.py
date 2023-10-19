1701c7564eb1701d7f054704f1c27713e61af824
eugder <eugder@gmail.com>
2023-10-19 14:00:27 +0300

    new topic tests added

--- teamglade/rooms/tests/test_view_new_topic.py
+++ teamglade/rooms/tests/test_view_new_topic.py
@@ -1,6 +1,6 @@
 from django.urls import reverse
 from django.test import TestCase
-from ..models import Room, RoomUser
+from ..models import Room, Topic, RoomUser
 
 
 class NewTopicTests(TestCase):
@@ -14,29 +14,30 @@ class NewTopicTests(TestCase):
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
 
-    # def test_new_topic_view_status_code2(self):
-    #     url = reverse('new_topic', kwargs={'pk': self.room.pk})
-    #     response = self.client.get(url)
-    #     self.assertEquals(response.status_code, 200)
+    def test_new_topic_view_status_code2(self):
+        url = reverse('new_topic', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_mscrf(self):
+        url = reverse('new_topic', kwargs={'pk': self.room.pk})
+        response = self.client.get(url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+    def test_new_topic_valid_post_data(self):
+        url = reverse('new_topic', kwargs={'pk': self.room.pk})
+        data = {'title': 'Test title', 'message': 'Test Message'}
+        self.client.post(url, data)
+        self.assertTrue(Topic.objects.exists())
 
 
+class LoginRequiredNewTopicTests(TestCase):
+    def setUp(self):
+        user = RoomUser.objects.create_user(username='usr', email='usr@test.com', password='111')
+        self.room = Room.objects.create(name='Room name', created_by=user)
+        self.url = reverse('new_topic', kwargs={'pk': self.room.pk})
+        self.response = self.client.get(self.url)
 
-
-    # These commented defs belongs NewTopicTests class
-    # def test_new_topic_view_status_code2(self):
-    #     url = reverse('new_topic', kwargs={'pk': 99})
-    #     response = self.client.get(url)
-    #     self.assertEquals(response.status_code, 404)
-
-    # def test_mscrf(self):
-    #     url = reverse('new_topic', kwargs={'pk': 1})
-    #     response = self.client.get(url)
-    #     self.assertContains(response, 'csrfmiddlewaretoken')
-
-
-
-    # def test_new_topic_valid_post_data(self):
-    #     url = reverse('new_topic', kwargs={'pk': 1})
-    #     data = {'title': 'Test title', 'message': 'Test Message'}
-    #     response = self.client.post(url, data)
-    #     self.assertTrue(Topic.objects.exists())
+    def test_redirection(self):
+        login_url = reverse('login')
+        self.assertRedirects(self.response, '{login_url}?next={url}'.format(login_url=login_url, url=self.url))
