1563d750a7a69a78873248bb0e960122c88d3612
eugder <eugder@gmail.com>
2023-11-30 14:03:45 +0200

    view topic tests added

---
+++ teamglade/rooms/tests/test_view_topic.py
@@ -0,0 +1,34 @@
+from django.urls import reverse, resolve
+from django.test import TestCase
+from ..models import Room, Topic, RoomUser
+from ..views import topic
+
+
+class TopicViewTests(TestCase):
+    def setUp(self):
+        user = RoomUser.objects.create_user(username='usr', email='usr@test.com', password='111')
+        room_obj = Room.objects.create(name='Room name', created_by=user)
+        topic_obj = Topic.objects.create(title='Test title', message='Test message', created_by=user, room=room_obj)
+        self.client.login(username='usr', password='111')  # topic view has a @login_required
+        self.url = reverse('topic', kwargs={'pk': topic_obj.pk})
+        self.response = self.client.get(self.url)
+
+    def test_topic_view_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_topic_view_contains_room_navigation_link(self):
+        homepage_url = reverse('room')
+        self.assertContains(self.response, 'href="{0}"'.format(homepage_url))
+
+    def test_url_resolves_correct_view(self):
+        view = resolve(self.url)
+        self.assertEquals(view.func, topic)
+
+class LoginRequiredTopicTests(TestCase):
+    def setUp(self):
+        self.url = reverse('topic', kwargs={'pk': 1})
+        self.response = self.client.get(self.url)
+
+    def test_redirection(self):
+        login_url = reverse('login')
+        self.assertRedirects(self.response, '{login_url}?next={url}'.format(login_url=login_url, url=self.url))
