7438943e5081a1f35403cb8b31eecfa1154656f9
eugder <eugder@gmail.com>
2024-03-21 14:02:22 +0200

    multiple files download feature adjusted

--- teamglade/rooms/admin.py
+++ teamglade/rooms/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import RoomUser, Room, Topic
+from .models import RoomUser, Room, Topic, File
+
+class FileInline(admin.TabularInline):
+    model = File
+
+
+class Admin(admin.ModelAdmin):
+    inlines = [
+        FileInline,
+    ]
 
 admin.site.register(RoomUser)
 admin.site.register(Room)
-admin.site.register(Topic)
+# admin.site.register(Topic)
+admin.site.register(Topic, Admin)
