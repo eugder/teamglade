1fef4192a51a0e9ac36a303cccd8e909b9d97264
eugder <eugder@gmail.com>
2024-05-07 14:03:02 +0300

    user update view room name field adding

--- teamglade/accounts/views.py
+++ teamglade/accounts/views.py
@@ -21,6 +21,19 @@ class RoomUserCreationForm(UserCreationForm):
     #     super(RoomUserCreationForm, self).__init__(*args, **kwargs)
     #     self.fields['email'].required = True
 
+class UserUpdateForm(forms.ModelForm):
+    room = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 5, 'placeholder': 'Your message'}),
+        label='Message',
+        max_length=1000,
+        help_text='The max length of the text is 1000.',
+    )
+
+    class Meta:
+        model = RoomUser
+        fields = ['username', 'email', 'room']
+
+
 @method_decorator(login_required, name='dispatch')
 class UserUpdateView(UpdateView):
     model = RoomUser
@@ -31,6 +44,18 @@ class UserUpdateView(UpdateView):
     def get_object(self):
         # let know UpdateView what exactly user is updating
         return self.request.user
+
+
+def user_update(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            return redirect('home')
+    else:
+        form = UserUpdateForm()
+    return render(request, 'signup.html', {'form': form})
 
 
 def signup(request):
