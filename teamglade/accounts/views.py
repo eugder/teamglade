4f270abd2633b6f358eb9fe8ba58c69acfce802b
eugder <eugder@gmail.com>
2025-05-30 14:09:30 +0300

    Email confirmation views token adding

--- teamglade/accounts/views.py
+++ teamglade/accounts/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
 from django.views.generic import UpdateView
 from rooms.models import RoomUser, Room
 from .forms import RoomUserCreationForm, UserUpdateForm
@@ -55,8 +60,17 @@ def signup(request):
             # all new users get a room
             new_room = Room.objects.create(
                 name=str(user.username) + " room",
-                created_by=user,
-            )
+                created_by=user,)
+
+            # token = default_token_generator.make_token(user)
+            # uid = urlsafe_base64_encode(force_bytes(user.pk))
+            #
+            # # generate link with identifier to login_invite view
+            # context = request.build_absolute_uri('/')[:-1] + reverse('login_invite', kwargs={'code': invite_code})
+            # html_message = render_to_string('invite_email.html', {'context': context, })
+            # subject = "[TeamGlade] You are invited to join TeamGlade room"
+            # message = EmailMessage(subject, html_message, to=[user.email])  # FROM field will be DEFAULT_FROM_EMAIL
+            # message.send()
 
             login(request, user)
             return redirect('home')
