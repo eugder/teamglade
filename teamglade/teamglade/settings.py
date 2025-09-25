4976652199521c2b3c64e3943e80f76d435ca1dd
eugder <eugder@gmail.com>
2025-09-25 13:16:18 +0300

    ready to deploy

--- teamglade/teamglade/settings.py
+++ teamglade/teamglade/settings.py
@@ -16,7 +16,7 @@ import os
 import logging
 
 # uncomment this for deployment on DigitalOcean
-# load_dotenv(dotenv_path='DO_prod.env')
+load_dotenv(dotenv_path='DO_prod.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
